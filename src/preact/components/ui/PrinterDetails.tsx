4ca58d3d547e2e36349cd45f13a78801075f667d
Luis Antonio López Rivera <luislopez2301@outlook.es>
2025-11-12 21:53:11 -0600

    refactor: Se ajusta la estructura de datos y la interfáz de usuario: - Se añade nueva información a los archivos JSON: Se añaden medidas en `cm` para todas las medidas de papel en `supported_paper_size` - Se ajustan las clases del navbar - Se añade funcionalidad responsive al navbar - Se ajusta la estructura de `PrinterTypes.ts` para intregrar los cambios realizados a la estructura de los JSON - Se añade nuevas descipciones más claras a `PrinterDetails.tsx` - Se crea un nuevo componente `PrinterCard.tsx` para manejar estados individuales, por cada modelo de impresora. Y se integra en `PrinterContainer.tsx`

--- src/preact/components/ui/PrinterDetails.tsx
+++ src/preact/components/ui/PrinterDetails.tsx
@@ -9,9 +9,19 @@ export default function PrinterDetails({ printer }: PrinterDetailsProps) {
   return (
     <div className='px-4'>
       <div>
-        <p className='text-center font-primary text-2xl font-bold bg-linear-to-r from-blue-500 to-teal-500 bg-clip-text text-transparent'>
-          {printer.model} | {printer.type}
-        </p>
+        <div className='font-primary mb-4 flex flex-row items-start justify-between'>
+          <div>
+            <p className='text-2xl font-bold bg-linear-to-r from-blue-500 to-teal-500 bg-clip-text text-transparent'>
+              {printer.model}
+            </p>
+            <p className='text-sm font-light dark:text-neutral-400'>
+              {printer.type}
+            </p>
+          </div>
+          <Badge className='font-semibold' variant='primary'>
+            {printer.brand}
+          </Badge>
+        </div>
         <div>
           <p className='font-bold'>Descripción</p>
           <p className='text-sm text-neutral-500 dark:text-neutral-400 leading-relaxed'>
@@ -29,36 +39,56 @@ export default function PrinterDetails({ printer }: PrinterDetailsProps) {
             <div className='grid grid-cols-2 gap-2'>
               <div className='p-3 bg-neutral-50 dark:bg-neutral-800 rounded-lg'>
                 <p className='text-xs text-neutral-500 dark:text-neutral-400'>
-                  PPM Negro
+                  PPM Negro (Modo ECO)
                 </p>
                 <p className='text-2xl font-bold text-neutral-900 dark:text-neutral-100'>
-                  {printer.printer_speed.ppm.black}
+                  {printer.printer_speed.ppm.black}{' '}
+                  <span className='text-xs'>máx.</span>
                 </p>
               </div>
               <div className='p-3 bg-neutral-50 dark:bg-neutral-800 rounded-lg'>
                 <p className='text-xs text-neutral-500 dark:text-neutral-400'>
-                  PPM Color
+                  PPM Color (Modo Eco)
                 </p>
                 <p className='text-2xl font-bold text-neutral-900 dark:text-neutral-100'>
-                  {printer.printer_speed.ppm.color}
+                  {printer.printer_speed.ppm.color}{' '}
+                  <span className='text-xs'>máx.</span>
                 </p>
               </div>
               <div className='p-3 bg-neutral-50 dark:bg-neutral-800 rounded-lg'>
                 <p className='text-xs text-neutral-500 dark:text-neutral-400'>
-                  IPM Negro
+                  IPM Negro (ISO/IEC 24734)
                 </p>
                 <p className='text-2xl font-bold text-neutral-900 dark:text-neutral-100'>
-                  {printer.printer_speed.ipm.black}
+                  {printer.printer_speed.ipm.black}{' '}
+                  <span className='text-xs'>máx.</span>
                 </p>
               </div>
               <div className='p-3 bg-neutral-50 dark:bg-neutral-800 rounded-lg'>
                 <p className='text-xs text-neutral-500 dark:text-neutral-400'>
-                  IPM Color
+                  IPM Color (ISO/IEC 24734)
                 </p>
                 <p className='text-2xl font-bold text-neutral-900 dark:text-neutral-100'>
-                  {printer.printer_speed.ipm.color}
+                  {printer.printer_speed.ipm.color}{' '}
+                  <span className='text-xs'>máx.</span>
                 </p>
               </div>
+            </div>
+            <div className='text-xs text-neutral-500 dark:text-neutral-400 mt-4 space-y-2'>
+              <p>
+                <strong>Modo Eco (PPM)</strong> se refiere a la velocidad máxima
+                de impresión que la máquina puede alcanzar cuando está
+                configurada en el modo más rápido y económico{' '}
+                <strong>(Borrador)</strong>.
+              </p>
+              <p>
+                <strong>IPM</strong> indica la velocidad de impresión siguiendo
+                la norma internacional{' '}
+                <strong>ISO/IEC 24734. Consideraciones:</strong> Este es el
+                estándar de la industria para la medición de velocidades y es
+                considerado el más exacto para comparar impresoras de diferentes
+                marcas.
+              </p>
             </div>
           </div>
 
@@ -92,13 +122,13 @@ export default function PrinterDetails({ printer }: PrinterDetailsProps) {
                 <span className='font-semibold text-neutral-900 dark:text-neutral-100'>
                   Contenedor estándar:
                 </span>{' '}
-                {printer.max_paper_capacity.standard_container}
+                {printer.max_paper_capacity.standard_container} hojas
               </p>
               <p className='text-sm text-neutral-500 dark:text-neutral-400'>
                 <span className='font-semibold text-neutral-900 dark:text-neutral-100'>
-                  Bypass:
+                  Ranura Manual (Bypass):
                 </span>{' '}
-                {printer.max_paper_capacity.bypass}
+                {printer.max_paper_capacity.bypass} hoja
               </p>
               <p className='text-sm text-neutral-500 dark:text-neutral-400'>
                 <span className='font-semibold text-neutral-900 dark:text-neutral-100'>
@@ -113,11 +143,54 @@ export default function PrinterDetails({ printer }: PrinterDetailsProps) {
 
           <div>
             <h3 className='text-lg font-semibold text-neutral-900 dark:text-neutral-100 mb-3 flex items-center gap-2'>
+              Gramaje de papel soportado
+            </h3>
+            <div className='space-y-2'>
+              <p className='text-sm text-neutral-500 dark:text-neutral-400'>
+                <span className='font-semibold text-neutral-900 dark:text-neutral-100'>
+                  Contenedor estándar:
+                </span>{' '}
+                {printer.supported_paper_weight_g_m2.standard_container}
+              </p>
+              <p className='text-sm text-neutral-500 dark:text-neutral-400'>
+                <span className='font-semibold text-neutral-900 dark:text-neutral-100'>
+                  Ranura Manual (Bypass):
+                </span>{' '}
+                {printer.supported_paper_weight_g_m2.bypass}
+              </p>
+            </div>
+          </div>
+          <hr className='border-neutral-500 dark:border-neutral-600' />
+
+          <div>
+            <h3 className='text-lg font-semibold text-neutral-900 dark:text-neutral-100 mb-3 flex items-center gap-2'>
               Tipos de papel soportados
             </h3>
             <p className='text-sm text-neutral-500 dark:text-neutral-400'>
               {printer.supported_paper_type}
             </p>
+          </div>
+
+          <hr className='border-neutral-500 dark:border-neutral-600' />
+
+          <div>
+            <h3 className='text-lg font-semibold text-neutral-900 dark:text-neutral-100 mb-3'>
+              Tamaños de papel soportados
+            </h3>
+            <div className='space-y-3'>
+              <div>
+                <p className='text-sm font-semibold text-neutral-900 dark:text-neutral-100 mb-2'>
+                  Contenedor estándar y Manual:
+                </p>
+                <div className='flex flex-wrap gap-2'>
+                  {printer.supported_paper_size.map((size, index) => (
+                    <Badge variant='primary' key={index}>
+                      {size}
+                    </Badge>
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
 
           <hr className='border-neutral-500 dark:border-neutral-600' />
@@ -129,15 +202,40 @@ export default function PrinterDetails({ printer }: PrinterDetailsProps) {
             <div className='space-y-2'>
               <p className='text-sm text-neutral-500 dark:text-neutral-400'>
                 <span className='font-semibold text-neutral-900 dark:text-neutral-100'>
-                  Máximo mensual:
+                  Ciclo Mensual de Trabajo Máximo:
                 </span>{' '}
                 {printer.max_month_job}
               </p>
               <p className='text-sm text-neutral-500 dark:text-neutral-400'>
                 <span className='font-semibold text-neutral-900 dark:text-neutral-100'>
-                  Recomendado mensual:
+                  Volumen Mensual Recomendado:
                 </span>{' '}
                 {printer.recommended_month_job}
+              </p>
+            </div>
+            <div className='text-xs text-neutral-500 dark:text-neutral-400 my-2 space-y-2'>
+              <p>
+                <strong>Volumen Mensual Recomendado</strong> Es el volumen de
+                impresión para el cual la impresora fue diseñada para trabajar
+                de manera constante, logrando la mejor relación costo-beneficio
+                y confiabilidad.
+              </p>
+              <p>
+                <strong>Ciclo Mensual de Trabajo Máximo</strong>
+                Este valor representa el límite superior de la capacidad de la
+                impresora y se utiliza principalmente como una métrica de
+                durabilidad y comparación. Definición: Es el número máximo de
+                páginas que la impresora puede imprimir en un mes sin sufrir
+                daños o desgastes prematuros. Es una medida de la robustez del
+                hardware.
+              </p>
+              <p>
+                <strong>Implicación:</strong> El ciclo máximo es un pico de
+                durabilidad y{' '}
+                <strong>no debe ser sostenido a lo largo del tiempo</strong>.
+                Imprimir constantemente cerca del ciclo máximo podría acortar la
+                vida útil de la impresora y requerir un mantenimiento más
+                frecuente.
               </p>
             </div>
           </div>
@@ -152,21 +250,43 @@ export default function PrinterDetails({ printer }: PrinterDetailsProps) {
               <section>
                 <div className='grid grid-cols-2 gap-2 mt-2'>
                   {Object.entries(printer.consumables).map(([color, data]) => (
-                    <div
-                      key={color}
-                      className='p-2 bg-neutral-50 dark:bg-neutral-800 rounded-md'
-                    >
+                    <Badge key={color}>
                       <p className='text-sm font-medium text-neutral-900 dark:text-neutral-100'>
                         <span className='font-semibold'>{data.model}</span>
-                        <span className='text-neutral-500 dark:text-neutral-400 ml-1'>
-                          ({data.yield_pages.toLocaleString()} pág.)
+                        <span className='text-neutral-500 dark:text-neutral-400 ml-1 text-xs'>
+                          ({data.yield_pages.toLocaleString()} pág. máx.)
                         </span>
                       </p>
-                    </div>
+                    </Badge>
                   ))}
                 </div>
               </section>
             )}
+            <div className='text-xs text-neutral-500 dark:text-neutral-400 my-2 space-y-2'>
+              <p>
+                El rendimiendo del (los) consumible (s) se calcula al 5% de
+                cobertura
+              </p>
+              <ul>
+                <li>
+                  El 5% es simplemente el punto de referencia acordado para que
+                  todas las marcas puedan decir "esta botella rinde X páginas".
+                </li>
+                <li>
+                  Si tus impresiones usan más tinta que ese 5% (por ejemplo,
+                  imprimes muchas tablas o fotos), la botella te durará menos
+                  páginas que el rendimiento anunciado.
+                </li>
+                <li>
+                  Si imprimes menos tinta que ese 5% (pocas líneas de texto), te
+                  durará más.
+                </li>
+              </ul>
+              <p>
+                Es una herramienta de comparación, no una garantía de uso
+                diario.
+              </p>
+            </div>
           </div>
 
           <hr className='border-neutral-500 dark:border-neutral-600' />
@@ -184,25 +304,23 @@ export default function PrinterDetails({ printer }: PrinterDetailsProps) {
 
           <hr className='border-neutral-500 dark:border-neutral-600' />
 
-          <div>
-            <h3 className='text-lg font-semibold text-neutral-900 dark:text-neutral-100 mb-3'>
-              Tamaños de papel soportados
-            </h3>
-            <div className='space-y-3'>
-              <div>
-                <p className='text-sm font-semibold text-neutral-900 dark:text-neutral-100 mb-2'>
-                  Contenedor estándar:
-                </p>
-                <div className='flex flex-wrap gap-2'>
-                  {printer.supported_paper_size.map((size, index) => (
-                    <Badge variant='primary' key={index}>
-                      {size}
-                    </Badge>
-                  ))}
-                </div>
-              </div>
+          {printer.technical_data_sheet_link && (
+            <div>
+              <h3 className='text-lg font-semibold text-neutral-900 dark:text-neutral-100 mb-3 flex items-center gap-2'>
+                Documentación
+              </h3>
+              <p className='text-sm text-blue-600 dark:text-blue-400'>
+                <a
+                  href={printer.technical_data_sheet_link}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='underline hover:no-underline font-semibold'
+                >
+                  Descargar Ficha Técnica Oficial
+                </a>
+              </p>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
